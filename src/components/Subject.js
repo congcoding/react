46ec5681cf0efc93d3b9ba243cf5f99aac07814a
congcoding <congcoding@gmail.com>
2020-05-25 21:12:27 +0900

    컴포넌트 제작

---
+++ src/components/Subject.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Subject extends Component {
+	render() {  //반드시 render가 있어야 함 (function 생략 가능)
+	  return (
+		//하나의 최상위 태그에서 시작해야 함
+		<header>
+		  {/*
+			<h1>WEB</h1>
+			world wide web!
+		  */}
+		  <h1>{this.props.title}</h1>
+		  {this.props.sub}
+			</header>
+	  );
+	}
+  }
+
+  export default Subject;
