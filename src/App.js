1f64d82af261c32310bdbd146069763d2d808aca
congcoding <congcoding@gmail.com>
2020-05-25 21:32:07 +0900

    state

--- src/App.js
+++ src/App.js
@@ -9,11 +9,27 @@ import './App.css';
 
 //유사 자바스크립트
 class App extends Component {
+  //render()보다 먼저 실행되면서, 그 Component를 초기화시켜주고 싶은 코드는 constructor()안에 코드를 작성한다.
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
